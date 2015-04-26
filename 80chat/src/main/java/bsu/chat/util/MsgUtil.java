0f953cb05fae8385c39dd98b585a29f6db4706ef
Pavel Pishchynski <pavelalexbel@outlook.com>
2015-04-26 14:56:25 +0300

    MsgUtil class edited

--- 80chat/src/main/java/bsu/chat/util/MsgUtil.java
+++ 80chat/src/main/java/bsu/chat/util/MsgUtil.java
@@ -4,25 +4,46 @@ package bsu.chat.util;
  * Created by Pavel Pishchynski on 26.04.2015.
  */
 
-import javax.servlet.http.HttpServletRequest;
-import java.io.BufferedReader;
-import java.io.IOException;
+import bsu.chat.model.Msg;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class MsgUtil {
-    public static final String APPLICATION_JSON = "application/json";
-    public  static final String TOKEN = "token";
-    private static final String MESSAGES = "messages";
+    public static final String TOKEN = "token";
+    public static final String MESSAGES = "messages";
+    private static final String TN = "TN";
+    private static final String EN = "EN";
+    private static final String ID = "id";
+    private static final String USERNAME = "username";
+    private static final String TEXT = "text";
 
     private MsgUtil(){
     }
 
-    public static String getMessageBody(HttpServletRequest request) throws IOException{
-        StringBuilder sb = new StringBuilder();
-        BufferedReader reader = request.getReader();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            sb.append(line);
-        }
-        return sb.toString();
+    public static String getToken(int index) {
+        Integer number = index * 8 + 11;
+        return TN + number + EN;
     }
+
+    public static int getIndex(String token) {
+        return (Integer.valueOf(token.substring(2, token.length() - 2)) - 11) / 8;
+    }
+
+    public static JSONObject stringToJson(String data) throws ParseException {
+        JSONParser parser = new JSONParser();
+        return (JSONObject) parser.parse(data.trim());
+    }
+
+    public static Msg jsonToTask(JSONObject json) {
+        Object id = json.get(ID);
+        Object user = json.get(USERNAME);
+        Object text = json.get(TEXT);
+
+        if (id != null && user != null && text != null) {
+            return new Msg((String) id, (String) user, (String) text);
+        }
+        return null;
+    }
+
 }
--- 80chat/src/main/java/bsu/chat/util/ServletUtil.java
+++ 80chat/src/main/java/bsu/chat/util/ServletUtil.java
@@ -1,7 +1,21 @@
 package bsu.chat.util;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
+
 /**
  * Created by Pavel Pishchynski on 26.04.2015.
  */
 public class ServletUtil {
+
+    public static String getMessageBody(HttpServletRequest request) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = request.getReader();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+        }
+        return sb.toString();
+    }
 }
