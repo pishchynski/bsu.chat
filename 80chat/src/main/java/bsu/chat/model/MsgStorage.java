af907655ceb731713b4a04cbd2389fafd540225f
Pavel Pishchynski <pavelalexbel@outlook.com>
2015-04-26 14:38:38 +0300

    MsgStorage class edited

--- 80chat/src/main/java/bsu/chat/model/MsgStorage.java
+++ 80chat/src/main/java/bsu/chat/model/MsgStorage.java
@@ -1,7 +1,49 @@
 package bsu.chat.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by Pavel Pishchynski on 26.04.2015.
  */
 public class MsgStorage {
+    private static List<Msg> storage = Collections.synchronizedList(new ArrayList<Msg>());
+
+    private MsgStorage(){
+    }
+
+    public static List<Msg> getStorage() {
+        return storage;
+    }
+
+    public static void addMsg(Msg msg){
+        storage.add(msg);
+    }
+
+    public static void addAll(Msg[] messages){
+        storage.addAll(Arrays.asList(messages));
+    }
+
+    public static int getSize(){
+        return storage.size();
+    }
+
+    public static Msg getMsgByIndex(int index){
+        return storage.get(index);
+    }
+
+    public static List<Msg> getSubMsgListByIndex(int index){
+        return storage.subList(index, storage.size());
+    }
+
+    public static Msg getMsgById(String id){
+        for(Msg msg: storage){
+            if(msg.getID().equals(id)){
+                return msg;
+            }
+        }
+        return null;
+    }
 }
